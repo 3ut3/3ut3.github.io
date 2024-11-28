52b48c9800bfe7e47d775da2bb5a1474b179270d
.ixx <monkey2ragon@gmail.com>
2024-11-28 19:08:32 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-// Dark Mode Toggle Script
-const toggleButton = document.createElement('button');
-toggleButton.innerText = '🌙 Toggle Dark Mode';
-document.body.appendChild(toggleButton);
-
-toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-});
